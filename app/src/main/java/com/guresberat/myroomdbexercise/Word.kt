bf40bfe70f20649eb7827f8fd23bdd964c38aae1
Beratcan Güreş <guresberatcan@gmail.com>
2021-08-14 22:18:05 +0300

    Initial commit

---
+++ app/src/main/java/com/guresberat/myroomdbexercise/Word.kt
@@ -0,0 +1,12 @@
+package com.guresberat.myroomdbexercise
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "word_table")
+class Word(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+
+    @ColumnInfo(name = "word") val word: String
+)
